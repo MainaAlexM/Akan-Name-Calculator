0d5662534216cd1ac64b1d25ba4e23639a6aa960
Mathenge-Alex <arxmaina@gmail.com>
2022-03-06 04:43:43 +0300

    add exact dat function in js evaluation function

--- js/index.js
+++ js/index.js
@@ -9,6 +9,9 @@ function evaluation(){
     let birthDay = d.getDay();
     alert(birthDay);
 
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let exactDay = days[birthDay];
+    alert(exactDay);
 }
 
 // const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
