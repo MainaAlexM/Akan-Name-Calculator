22c3dd91da63221ab964156f7889b6b22c00200d
Mathenge-Alex <arxmaina@gmail.com>
2022-03-06 07:11:02 +0300

    file: edit readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Akan-Name-Calculator
-This site helps calculate Akan names depending on one's Birthday.
+This site helps calculate Akan names relative to theirs depending on one's Birthday.
+
+### Author Name 
+This project was made by **Alex Mathenge** through his learning at Moringa school.  
+
+## Project description
+This project is not just a replica of just any other Akan name project but entails  a well thought improvement of different approaches to solve the Arkan Names. The approach used to develop this project enhances a higher accuracy in calculation of the dates and name resolution.
+
+## Usage
+Users can interact with this project over the internet by clicking on the link provided at the description of this project or by cloning the code into their local hosts. You can also click this [Website Link](https://mathenge-alex.github.io/Akan-Name-Calculator/).
+
+## Project Setup and Installation Instructions 
+The project's code can be downloaded, forked or cloned by individuals for learning or development purposes as governed by the MIT licence. I do not give any warranty for usage.
+
+## Behavior Driven Development (BDD).
+The project was developed on a behavior-driven development approach. This methods focuses on defining what the system should do and outlining a sample of relevant inputs and their expected outputs.
+
+## Technologies Used.
+The project uses technologies such as:
+- Javascript
+- Html5
+- CSS3
+- Git
+
+## Contat Information
+For more information, you can contact Alex Mathenge on arxmaina@gmail.com
+
+## Copyright and Licensing 
+copyright @ 2022 Alex Mathenge. MIT Licence.
--- js/index.js
+++ js/index.js
@@ -1,44 +1,44 @@
 function formReset(){
-    document.getElementById("form_data").reset();
+    document.getElementById('form-data').reset();
 }
 
 function evaluation(){
+    document.getElementById('birthDay')
     var day = document.getElementById("birthDay").value;
-    var sex = document.getElementById("gender").value;
-    alert(sex);
+    var gender = document.getElementById("gender").value;
+    var outputMessage;
+    alert(day);
 
-    const d = new Date(day);
+        // Get the day index (0-6) 0 = sunday
+    const d = new Date(day);    // Date(01 June 2010)
     let birthDay = d.getDay();
-    alert(birthDay);
-
+    
+        // Determine the exact day using index
     const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let exactDay = days[birthDay];
-    alert(exactDay);
-
-        // akan Names
+    alert(typeof exactDay);
+    
+        // Use index to determine the Arkan name
     const maleAkans = ["Kwasi"," Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     const femaleAkans = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-
-            // Aka determination Based on gender
-    if (sex = "male"){
-    	let akan = maleAkans[birthDay];
-      alert("You are male and your akan Name is " + akan);
-    } else if (sex = "female"){
-    	let akan = femaleAkans[birthDay];
-    	alert("You are female and your akan Name is " + akan);
+    
+    // document.getElementById("result").innerHTML = "You were born on " + exactDay;
+    
+        // Gender evaluation logic
+    if (gender == "male" && typeof d != "undefined"){
+      let akan = maleAkans[birthDay];
+      outputMessage = "You are male, born on " + exactDay + ". Your akan Name is " + akan + ".";
+    } 
+    else if (gender == "female" && typeof d != "undefined"){
+      let femaleAkan = femaleAkans[birthDay];
+      outputMessage = "You are female, born on " + exactDay + ". Your akan Name is " + femaleAkan + ".";
+    }
+    else if (typeof exactDay === "undefined" || gender == "default"){
+        outputMessage = "Please select your gender and select a valid date";
     }
 
-        // Name evaluation logic
+        // Display the Result
+    document.getElementById("result").innerHTML = outputMessage;
 
-        if (gender == "male"){
-            let akan = maleAkans[birthDay];
-            alert("You are male and your akan Name is " + akan);
-          } 
-          else if (gender == "female"){
-            let femaleAkan = femaleAkans[birthDay];
-            alert("You are female and your akan Name is " + femaleAkan);
-          }
-          else{
-          alert("Please select your gender and select a valid date");
-          }
+    formReset();
 }
