ef7e00b46c7f5ff65aedb2e22a0af1628088b760
Mathenge-Alex <arxmaina@gmail.com>
2022-03-06 05:09:25 +0300

    add akan name evaluation logic in evaluation function

--- js/index.js
+++ js/index.js
@@ -14,6 +14,34 @@ function evaluation(){
     const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let exactDay = days[birthDay];
     alert(exactDay);
+
+        // akan Names
+    const maleAkans = ["Kwasi"," Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    const femaleAkans = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+            // Aka determination Based on gender
+    if (sex = "male"){
+    	let akan = maleAkans[birthDay];
+      alert("You are male and your akan Name is " + akan);
+    } else if (sex = "female"){
+    	let akan = femaleAkans[birthDay];
+    	alert("You are female and your akan Name is " + akan);
+    }
+
+        // Name evaluation logic
+
+        if (gender = "female"){
+            let femaleAkan = femaleAkans[birthDay];
+            alert("You are female and your akan Name is " + femaleAkan);
+          }
+             else if (gender = "male"){
+            let akan = maleAkans[birthDay];
+            alert("You are male and your akan Name is " + akan);
+          } 
+          else{
+          alert("Please select your gender and select a valid date");
+          }
+      
 }
 
 // const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
